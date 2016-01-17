5ead25131a36534e5c52b386554c3113ff070490
Eleonore9 <eleonore.mayola@ymail.com>
2016-01-17 18:06:34 +0000

    WIP: ajax call to get all potion-making data

---
+++ PotionBook.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+  console.log( "HELLO!" );
+  getAllData();
+});
+
+
+function getAllData() {
+  // Get all program. languages info
+  $.get("https://potion-making.herokuapp.com/all-data").done(function(data)
+							     {data;})
+}
