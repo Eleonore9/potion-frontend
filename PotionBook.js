7e2a24304bf7197149b7e62b9e6e0b864e99d433
Eleonore9 <eleonore.mayola@ymail.com>
2016-01-18 20:07:23 +0000

    Y U say type "undefined" o_O

--- PotionBook.js
+++ PotionBook.js
@@ -1,15 +1,19 @@
 $( document ).ready(function() {
   console.log( "HELLO!" );
-  getAllData();
+  var allData = getAllData();
+  console.log(typeof allData)
+  console.log( "Bye!" );
 });
 
 
 function getAllData() {
   // Get all program. languages info
-  $.get("https://potion-making.herokuapp.com/all-data").done(function(data){ var allData = data;
-							for (i = 0; i < allData.length; i++){
-							  console.log(allData[i]);
-							}
-						      }
-				     );
+  $.getJSON(//"https://potion-making.herokuapp.com/all-data"
+    "http://localhost:5000/all-data",
+    function( data ){
+      data;
+    });
+    // .done(function(data){
+    // 	 data;
+    //    });
 }
