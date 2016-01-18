b804dcfd014437336fec3c568323a42cdc649d0a
Eleonore9 <eleonore.mayola@ymail.com>
2016-01-18 11:32:48 +0000

    Test with live potion-making app

--- PotionBook.js
+++ PotionBook.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
 
 function getAllData() {
   // Get all program. languages info
-  $.get("http://localhost:5000/all-data").done(function(data){ var allData = data;
+  $.get("https://potion-making.herokuapp.com/all-data").done(function(data){ var allData = data;
 							for (i = 0; i < allData.length; i++){
 							  console.log(allData[i]);
 							}
