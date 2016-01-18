c96df02db2a951ec67a692de8a61be9570bf1775
Eleonore9 <eleonore.mayola@ymail.com>
2016-01-18 11:09:04 +0000

    Fix syntax

--- PotionBook.js
+++ PotionBook.js
@@ -6,12 +6,11 @@ $( document ).ready(function() {
 
 function getAllData() {
   // Get all program. languages info
-  $.get(//"https://potion-making.herokuapp.com/all-data"
-	"http://localhost:5000/all-data"
-	{ signature: authHeader }).done(function(data){ var allData = data;
+  $.get("http://localhost:5000/all-data",
+	{signature: authHeader}).done(function(data){ var allData = data;
 							for (i = 0; i < allData.length; i++){
 							  console.log(allData[i]);
 							}
 						      }
-				       )
+				     );
 }
