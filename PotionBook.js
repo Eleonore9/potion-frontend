56dbf907897f457f60e719db2f4a1fa9e02fc0a6
Eleonore9 <eleonore.mayola@ymail.com>
2016-01-20 20:22:53 +0000

    Can parse json + display languages names

--- PotionBook.js
+++ PotionBook.js
@@ -1,19 +1,25 @@
 $( document ).ready(function() {
   console.log( "HELLO!" );
-  var allData = getAllData();
-  console.log(typeof allData)
-  console.log( "Bye!" );
+  getAllData();
+  console.log( "BYE!" );
 });
 
 
 function getAllData() {
-  // Get all program. languages info
-  $.getJSON(//"https://potion-making.herokuapp.com/all-data"
-    "http://localhost:5000/all-data",
-    function( data ){
-      data;
+  // Get all programming languages info
+  $.getJSON( "http://localhost:5000/all-data", function() {
+  }).done(function(data) {
+    console.log( "SUCCESS" );
+    $.each( data, function( key, val ) {
+      $(".list-group").append('<li class="list-group-item">' + val.name + "</li>");
     });
-    // .done(function(data){
-    // 	 data;
-    //    });
+  })
+  .fail(function(allData, textStatus, error) {
+    console.log( "error" );
+    var err = textStatus + ", " + error;
+    console.log( "Request Failed: " + err );
+  })
 }
+
+
+// "https://potion-making.herokuapp.com/all-data"
