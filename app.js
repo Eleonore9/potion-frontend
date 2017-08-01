53326f0b3484cc4df3441658c72c1ff2b1f1c3c1
Eleonore Mayola (Eleonore9) <eleonore.mayola@ymail.com>
2017-08-02 00:20:21 +0200

    Display filters

--- app.js
+++ app.js
@@ -5,4 +5,21 @@ angular.module('potionBook', [])
       .then(function(response) {
 	book.recipes = response.data;
       });
+    $http.get("https://potion-making.herokuapp.com/all-paradigms")
+      .then(function(response) {
+	book.paradigms = response.data;
+      });
+    $http.get("https://potion-making.herokuapp.com/all-types")
+      .then(function(response) {
+	book.types = response.data;
+      });
+    $http.get("https://potion-making.herokuapp.com/all-uses")
+      .then(function(response) {
+	book.uses = response.data;
+      });
+    book.getFilters = function(UserInput) {
+      if(UserInput == 'paradigms'){ book.filters = book.paradigms; }
+      else if(UserInput == 'types'){ book.filters = book.types; }
+      else if(UserInput == 'uses'){ book.filters = book.uses; }
+    }
   });
