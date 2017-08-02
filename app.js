3940680ba132bf1cefae31723627565053b60421
Eleonore Mayola (Eleonore9) <eleonore.mayola@ymail.com>
2017-08-03 00:42:02 +0200

    WIP: New fn to apply filters

--- app.js
+++ app.js
@@ -17,9 +17,31 @@ angular.module('potionBook', [])
       .then(function(response) {
 	book.uses = response.data;
       });
-    book.getFilters = function(UserInput) {
-      if(UserInput == 'paradigms'){ book.filters = book.paradigms; }
-      else if(UserInput == 'types'){ book.filters = book.types; }
-      else if(UserInput == 'uses'){ book.filters = book.uses; }
+    book.getFilters = function(userInput) {
+      if(userInput == 'paradigms'){ book.filters = book.paradigms; }
+      else if(userInput == 'types'){ book.filters = book.types; }
+      else if(userInput == 'uses'){ book.filters = book.uses; }
     }
+
+    book.selectRecipes = function(filterOption){
+      if(book.paradigms.indexOf(filterOption) != -1){
+	console.log(1);
+    	book.recipes = book.recipes.filter(function(e){
+    	                 e.paradigm == filterOption;
+	               });
+      }
+      else if(book.types.indexOf(filterOption) != -1){
+	console.log(2);
+    	book.recipes = book.recipes.filter(function(e){
+    	                 e.type == filterOption;
+	               });
+      }
+      else if(book.uses.indexOf(filterOption) != -1){
+	console.log(3);
+    	book.recipes = book.recipes.filter(function(e){
+    	                 e.use.indexOf(filterOption);
+	               });
+      }
+    }
+
   });
