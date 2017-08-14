d65894e7ee4b512a33f53f3937bf799eee38b537
Eleonore Mayola (Eleonore9) <eleonore.mayola@ymail.com>
2017-08-14 13:27:30 +0200

    WIP: implement angular.filter() fn

--- app.js
+++ app.js
@@ -23,25 +23,52 @@ angular.module('potionBook', [])
       else if(userInput == 'uses'){ book.filters = book.uses; }
     }
 
-    book.selectRecipes = function(filterOption){
-      if(book.paradigms.indexOf(filterOption) != -1){
-      	book.debug = 1;
-      	book.recipes = book.recipes.filter(function(e){
-      	                 return e.paradigm === filterOption;
-      	               });
-      }
-      else if(book.types.indexOf(filterOption) != -1){
-      	book.debug = 2;
-      	book.recipes = book.recipes.filter(function(e){
-      	                 return e.type === filterOption;
-      	               });
-      }
-      else if(book.uses.indexOf(filterOption) != -1){
-      	book.debug = 3;
-      	book.recipes = book.recipes.filter(function(e){
-      	                 return e.use.indexOf(filterOption);
-      	               });
-      }
+    book.updateFilterOption = function(userInput) {
+      book.debug = "foo";
+      book.filterOption = userInput;
     }
+
+    book.filter('selectRecipes', function(allRecipes, filterOption){
+      book.debug = 0;
+      if (!filterOption){ return allRecipes; }
+      var result = [];
+      angular.forEach(allRecipes, function(recipe){
+	if(book.paradigms.indexOf(filterOption) != -1 && recipe.paradigm === filterOption){
+      	  book.debug = 1;
+      	  result.push(recipe);
+        }
+        else if(book.types.indexOf(filterOption) != -1 && recipe.type === filterOption){
+      	  book.debug = 2;
+      	  result.push(recipe);
+	}
+	else if(book.uses.indexOf(filterOption) != -1 && recipe.use.indexOf(filterOption)){
+      	  book.debug = 3;
+      	  result.push(recipe);
+	}
+      });
+      return result;
+    });
+
+
+    // book.selectRecipes = function(filterOption){
+    //   if(book.paradigms.indexOf(filterOption) != -1){
+    //   	book.debug = 1;
+    //   	book.recipes = book.recipes.filter(function(e){
+    //   	                 return e.paradigm === filterOption;
+    //   	               });
+    //   }
+    //   else if(book.types.indexOf(filterOption) != -1){
+    //   	book.debug = 2;
+    //   	book.recipes = book.recipes.filter(function(e){
+    //   	                 return e.type === filterOption;
+    //   	               });
+    //   }
+    //   else if(book.uses.indexOf(filterOption) != -1){
+    //   	book.debug = 3;
+    //   	book.recipes = book.recipes.filter(function(e){
+    //   	                 return e.use.indexOf(filterOption);
+    //   	               });
+    //   }
+    // }
 
   });
